5b24bb3b27a27154a5ad07f3d246cf4f9d4c02b8
Mike Rinehart <mike_rinehart@rapid7.com>
2019-06-24 15:25:54 -0500

    Update contact, add plugin spec parser

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ setup(name='insightconnect_integrations_validators',
       version='0.1.0',
       description='Validator tooling for InsightConnect integrations',
       author='Rapid7 Integrations Alliance',
-      author_email='integrationsalliance@rapid7.com',
+      author_email='integrationalliance@rapid7.com',
       url='https://github.com/rapid7/icon-integrations-validators',
       packages=find_packages(),
       install_requires=[
           'validators==0.12.1',
           'filetype==1.0.0',
-          'pathlib==1.0.1'
+          'pathlib==1.0.1',
+          'insightconnect-integrations-plugin-spec-tooling==1.0.0'
       ],
       license="MIT"
       )
