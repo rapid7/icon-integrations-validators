1652c8b1ef0be98cd37d963686a02e0e3678943b
Mike Rinehart <mike_rinehart@rapid7.com>
2019-06-26 11:23:53 -0500

    Use proper return code

--- icon_validator/__main__.py
+++ icon_validator/__main__.py
@@ -6,8 +6,8 @@ def main():
     args = sys.argv
 
     if not len(args) >= 2:
-        print("Invalid call")
-        exit(0)
+        print(f"Missing plugin directory to validate! Example: icon-validate my_plugin/")
+        exit(1)
 
     print(f"Validating plugin at {args[1]}")
     validate(directory=args[1])
