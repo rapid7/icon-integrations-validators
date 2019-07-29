b8910adc63d0d6cc29564d36d3350d9ae7915df3
Jakob Herlitz <jakob_herlitz@rapid7.com>
2019-07-29 17:01:12 -0400

    added checks for all titles and descriptions

--- rules/acronym_validator.py
+++ rules/acronym_validator.py
@@ -4,6 +4,7 @@ from .validator import KomandPluginValidator
 class AcronymValidator(KomandPluginValidator):
 
     acronyms = ['API', 'AWS', 'BCC', 'BIOS', 'CI', 'CLI', 'CPU', 'CRLF', 'CSV', 'FTP', 'GMT', 'GNU', 'GPU', 'HTML', 'HTTP', 'HTTPS', 'ID', 'IMAP', 'IO', 'IP', 'IP2', 'ISO', 'JPEG', 'JQ', 'JQL', 'JSON', 'LAN', 'MD5', 'MIME', 'PDF', 'PHP', 'PID', 'PNG', 'REST', 'RPM', 'RRS', 'RSA', 'SDK', 'SHA', 'SHA1', 'SHASUM', 'SHA1SUM', 'SHA256', 'SHA512', 'SMS', 'SMTP', 'SQL', 'SSH', 'SSL', 'TCP', 'UDP', 'UI', 'UID', 'URI', 'URL', 'UUID', 'VPN', 'XML', 'ZIP']
+    
 
     @staticmethod
     def validate_acronym(s):
@@ -12,6 +13,27 @@ class AcronymValidator(KomandPluginValidator):
                 if c.isalpha() and not c.isupper():
                     return True
         return False
+    
+    @staticmethod
+    def validate_subsection(section, bad): 
+        if type(section) is not dict:
+            return
+        try:
+            desc = section['description']
+            AcronymValidator.validate_line(desc.split(), bad)
+            # items in spec sometimes have a desc but no title
+            title = section['title']
+            AcronymValidator.validate_line(title.split(), bad)
+        except KeyError:
+            pass 
+        for subsection in section:
+                AcronymValidator.validate_subsection(section[subsection], bad) 
+
+    @staticmethod
+    def validate_line(content, bad):
+        for word in content:
+            if AcronymValidator.validate_acronym(word):
+                bad.append(word)
 
     def validate(self, spec):
         bad = []
@@ -21,9 +43,12 @@ class AcronymValidator(KomandPluginValidator):
                 content = spec.raw_help().split()
             else:
                 content = spec.spec_dictionary()[section].split()
-            for word in content:
-                if AcronymValidator.validate_acronym(word):
-                    bad.append(word)
+            AcronymValidator.validate_line(content, bad)
+        
+        subsections = ['actions', 'triggers', 'connection', 'types']
+        for section in subsections: 
+            if section in spec.spec_dictionary():
+                AcronymValidator.validate_subsection(spec.spec_dictionary()[section], bad)
         
 
         if len(bad) > 0:
