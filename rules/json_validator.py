91c162b71b503541a58a92066484c7aef2fcc17e
Jakob Herlitz <jakob_herlitz@rapid7.com>
2019-08-30 12:31:30 -0400

    simplified code

--- rules/json_validator.py
+++ rules/json_validator.py
@@ -6,19 +6,15 @@ import json
 class JSONValidator(KomandPluginValidator):
     invalid_files = []
 
-    @staticmethod
-    def validate_json(text, name):
-        try:
-            json.loads(text)
-        except json.decoder.JSONDecodeError:
-            JSONValidator.invalid_files.append(name)
-
     def validate(self, spec):
         tests_dir = os.path.join(spec.directory, "tests")
         for path, _, files in os.walk(tests_dir):
             for name in files:
                 if name.endswith(".json"):
                     with open(os.path.join(tests_dir, name)) as test:
-                        JSONValidator.validate_json(test.read(), name)
+                        try:
+                            json.load(test)
+                        except json.decoder.JSONDecodeError:
+                            JSONValidator.invalid_files.append(name)
         if len(JSONValidator.invalid_files) > 0:
             raise Exception(f"The following test files are not in proper JSON format: {JSONValidator.invalid_files}")
