0847c1497fa8e451510cd37155f310f15da8f1dc
Jakob Herlitz <jakob_herlitz@rapid7.com>
2019-09-05 10:15:11 -0400

    fixed typo - checked for any type of Exception outside of allowed kind - added __init__ for _violating_files variable

--- rules/exception_validator.py
+++ rules/exception_validator.py
@@ -1,26 +1,32 @@
 from .validator import KomandPluginValidator
 import os
+import re
 
 
 class ExceptionValidator(KomandPluginValidator):
-    violating_files = []
+    def __init__(self):
+        super().__init__()
+        self._violating_files = []
 
-    @staticmethod
-    def validate_exceptions(spec_dir, path, name):
+    def validate_exceptions(self, spec_dir, path, name):
         joined_path = os.path.join(path, name)
         with open(joined_path, 'r') as f:
             text = f.read()
-        if "raise Exception" in text:
-            ExceptionValidator.violating_files.append(os.path.relpath(joined_path, spec_dir))
+        pattern = "raise [A-Za-z]*"
+        matches = re.findall(pattern, text)
+        violations = list(filter(lambda x: "raise PluginException" not in x
+                                           and "raise ConnectionTestException" not in x, matches))
+        if len(violations) > 0:
+            self._violating_files.append(os.path.relpath(joined_path, spec_dir))
 
     def validate(self, spec):
         d = spec.directory
         for path, _, files in os.walk(d):
             for name in files:
                 if name.endswith(".py"):
-                    ExceptionValidator.validate_exceptions(d, path, name)
-        if len(ExceptionValidator.violating_files) > 0:
-            raise Exception(f"Please use 'PluginException' and"
+                    self.validate_exceptions(d, path, name)
+        if len(self._violating_files) > 0:
+            raise Exception(f"Please use 'PluginException' or"
                             f" 'ConnectionTestException' instead of 'Exception'. "
-                            f"The following files violated this rule: {ExceptionValidator.violating_files}")
+                            f"The following files violated this rule: {self._violating_files}")
 
