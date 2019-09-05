8fa9f5fe0d439ceb05d40b7fff8ac3806e7805e3
Jakob Herlitz <jakob_herlitz@rapid7.com>
2019-09-05 16:46:44 -0400

    added check for other credential type

--- rules/credentials_validator.py
+++ rules/credentials_validator.py
@@ -11,16 +11,29 @@ class CredentialsValidator(KomandPluginValidator):
             for name in list(filter(lambda x: x.endswith(".json"), files)):
                 with open(os.path.join(tests_dir, name)) as test:
                     data = json.load(test)
+                    added = False
                     try:
                         creds = data.get("body").get("connection").get("credentials")
-                        if creds is None:
-                            continue
-                        else:
+                        if creds is not None:
                             for key in creds:
                                 if creds[key] != "":
                                     violating_files.append(f"tests/{name}")
-                                    continue
+                                    added = True
+                                    break
                     except AttributeError:
+                        pass
+
+                    if added:
                         continue
+
+                    try:
+                        creds = data.get("body").get("connection").get("username_password")
+                        if creds is not None:
+                            for key in creds:
+                                if creds[key] != "":
+                                    violating_files.append(f"tests/{name}")
+                                    break
+                    except AttributeError:
+                        pass
         if len(violating_files) > 0:
             raise Exception(f"Remove credentials from the following files: {violating_files}")
