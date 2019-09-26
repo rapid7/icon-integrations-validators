bde0a1269d660ecf175e74a2f893a1a4d5b3de47
Jakob Herlitz <jakob_herlitz@rapid7.com>
2019-09-26 10:28:43 -0400

    added line number error messages in exception validator

--- rules/exception_validator.py
+++ rules/exception_validator.py
@@ -11,13 +11,19 @@ class ExceptionValidator(KomandPluginValidator):
     def validate_exceptions(self, spec_dir, path, name):
         joined_path = os.path.join(path, name)
         with open(joined_path, 'r') as f:
-            text = f.read()
+            text = f.readlines()
+
+        violating_lines = []
         pattern = "raise [A-Za-z]*"
-        matches = re.findall(pattern, text)
-        violations = list(filter(lambda x: "raise PluginException" not in x
-                                           and "raise ConnectionTestException" not in x, matches))
-        if len(violations) > 0:
-            self._violating_files.append(os.path.relpath(joined_path, spec_dir))
+        for line_num in range(len(text)):
+            matches = re.findall(pattern, text[line_num])
+            violations = list(filter(lambda x: "raise PluginException" not in x
+                                               and "raise ConnectionTestException" not in x, matches))
+            if len(violations) > 0:
+                violating_lines.append(line_num)
+
+        if len(violating_lines) > 0:
+            self._violating_files.append((os.path.relpath(joined_path, spec_dir), f"Line Number(s): {violating_lines}"))
 
     def validate(self, spec):
         d = spec.directory
@@ -27,6 +33,6 @@ class ExceptionValidator(KomandPluginValidator):
                     self.validate_exceptions(d, path, name)
         if len(self._violating_files) > 0:
             raise Exception(f"Please use 'PluginException' or"
-                            f" 'ConnectionTestException' instead of 'Exception'. "
+                            f" 'ConnectionTestException' when raising an exception. "
                             f"The following files violated this rule: {self._violating_files}")
 
