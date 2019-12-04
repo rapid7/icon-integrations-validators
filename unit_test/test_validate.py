aaf3b2e1cb64fe79891e23f705d75e1c887fed2e
Joey McAdams <jmcadams@rapid7.com>
2019-12-04 15:23:17 -0600

    cleaning up unit test

--- unit_test/test_validate.py
+++ unit_test/test_validate.py
@@ -6,6 +6,6 @@ class TestValidate(TestCase):
     def test_validate(self):
         # This assumes a standard environment where everything is
         # go/src/github.com/rapid7/<projects>
-        directory_to_test = "../../insightconnect-plugins/microsoft_teams"
+        directory_to_test = "../../komand-plugins/example/"
         file_to_test = "plugin.spec.yaml"
         validate(directory_to_test, file_to_test, False, True)
