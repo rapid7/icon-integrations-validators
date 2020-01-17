d68824ddeedcacd2429994c3d27a20fef6001c29
matt hofert <mhofert@rapid7.com>
2020-01-17 12:54:01 -0600

    update test

--- unit_test/test_validate.py
+++ unit_test/test_validate.py
@@ -3,9 +3,14 @@ from icon_validator.validate import validate
 
 
 class TestValidate(TestCase):
-    def test_validate(self):
-        # This assumes a standard environment where everything is
-        # go/src/github.com/rapid7/<projects>
-        directory_to_test = "../../komand-plugins/example/"
+    def test_plugin_validate(self):
+        # example workflow in plugin_example directory. Run tests with these files
+        directory_to_test = "plugin_example"
         file_to_test = "plugin.spec.yaml"
+        validate(directory_to_test, file_to_test, False, True)
+
+    def test_workflow_validate(self):
+        # example workflow in workflow_example directory. Run tests with these files
+        directory_to_test = "workflow_example"
+        file_to_test = "workflow.spec.yaml"
         validate(directory_to_test, file_to_test, False, True)
