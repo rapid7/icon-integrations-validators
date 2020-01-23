296111415fec9aa5d65b24810e695f48787ab059
matt hofert <mhofert@rapid7.com>
2020-01-23 11:01:53 -0600

    refactor

--- icon_validator/validate.py
+++ icon_validator/validate.py
@@ -11,56 +11,48 @@ from .styling import *
 from .timing import *
 
 
-class Validate:
+def validate(directory, spec_file_name="plugin.spec.yaml", fail_fast=False, run_all=False, validators=list(), unit_test=False):
+    spec = KomandPluginSpec(directory, spec_file_name)
+    status = 0  # Resultant return code
+    start_time = time_now()
+    print(f"{BULLET_OK} {BOLD}Running Integration Validators...{CEND}")
 
-    def __init__(self, validators=None, unit_test=False):
-        if validators is None:
-            validators = list()
-        self.validators = validators
-        self.unit_test = unit_test  # Used to stop some of the print outs during unit testing, to increase legibility
+    if not validators:
+        if spec_file_name == "plugin.spec.yaml":
+            validators = VALIDATORS
+            if run_all:
+                validators += JENKINS_VALIDATORS
+        elif spec_file_name == "workflow.spec.yaml":
+            validators = WORKFLOW_VALIDATORS
 
-    def validate(self, directory, spec_file_name="plugin.spec.yaml", fail_fast=False, run_all=False):
-        spec = KomandPluginSpec(directory, spec_file_name)
-        status = 0  # Resultant return code
-        start_time = time_now()
-        print(f"{BULLET_OK} {BOLD}Running Integration Validators...{CEND}")
+    for v in validators:
+        print(f"{BULLET_OK} Executing validator {v.name}")
+        try:
+            v.validate(spec)
+            success = True
 
-        if not self.validators:
-            if spec_file_name == "plugin.spec.yaml":
-                self.validators = VALIDATORS
-                if run_all:
-                    self.validators += JENKINS_VALIDATORS
-            elif spec_file_name == "workflow.spec.yaml":
-                self.validators = WORKFLOW_VALIDATORS
+        except ValidationException as e:
+            if not unit_test:
+                print(f"Validator {v.name} failed!")
+                ex_type, ex, tb = sys.exc_info()
+                traceback.print_exception(Exception, e, tb)
+            else:
+                print(e)
+            status = 1
+            success = False
 
-        for v in self.validators:
-            print(f"{BULLET_OK} Executing validator {v.name}")
-            try:
-                v.validate(spec)
-                success = True
+        if not success and fail_fast:
+            break
 
-            except ValidationException as e:
-                if not self.unit_test:
-                    print(f"Validator {v.name} failed!")
-                    ex_type, ex, tb = sys.exc_info()
-                    traceback.print_exception(Exception, e, tb)
-                else:
-                    print(e)
-                status = 1
-                success = False
+    end_time = time_now()
+    time_elapsed = format_time(start=start_time, end=end_time)
 
-            if not success and fail_fast:
-                break
+    extension = spec_file_name.split(".")[0].capitalize()
 
-        end_time = time_now()
-        time_elapsed = format_time(start=start_time, end=end_time)
+    if status == 0:
+        print(f"{BULLET_OK} {BOLD}{extension} successfully validated!{CEND}")
+    else:
+        print(f"{BULLET_FAIL}{extension} failed validation!")
 
-        extension = spec_file_name.split(".")[0].capitalize()
-
-        if status == 0:
-            print(f"{BULLET_OK} {BOLD}{extension} successfully validated!{CEND}")
-        else:
-            print(f"{BULLET_FAIL}{extension} failed validation!")
-
-        print(f"\n----\n{BULLET_OK}{BOLD} Total time elapsed: {time_elapsed}ms{CEND}")
-        return status
+    print(f"\n----\n{BULLET_OK}{BOLD} Total time elapsed: {time_elapsed}ms{CEND}")
+    return status
--- unit_test/test_validate.py
+++ unit_test/test_validate.py
@@ -1,5 +1,5 @@
 import unittest
-from icon_validator.validate import Validate
+from icon_validator.validate import validate
 
 # Import validators to pass to tests
 from icon_validator.rules.workflow_validators.workflow_profanity_validator import WorkflowProfanityValidator
@@ -16,82 +16,72 @@ class TestPluginValidate(unittest.TestCase):
 
     def test_plugin_validate(self):
         # example workflow in plugin_examples directory. Run tests with these files
-        validater = Validate()
         directory_to_test = "plugin_examples"
         file_to_test = "plugin.spec.yaml"
-        validater.validate(directory_to_test, file_to_test, False, True)
+        validate(directory_to_test, file_to_test, False, True)
 
 
 class TestWorkflowValidate(unittest.TestCase):
 
     def test_good_workflow_validator(self):
         # Test good workflow. This should pass all validation
-        validater = Validate()
         directory_to_test = "workflow_examples/good_test"
         file_to_test = "workflow.spec.yaml"
-        result = validater.validate(directory_to_test, file_to_test, False, True)
+        result = validate(directory_to_test, file_to_test, False, True)
         self.assertFalse(result)
 
     def test_vendor_validator(self):
         # Test bad workflows. This will test the workflow_vendor_validator
-        validater = Validate([WorkflowVendorValidator()], True)
         directory_to_test = "workflow_examples/vendor_tests"
         file_to_test = "workflow_no_vendor.spec.yaml"
-        result = validater.validate(directory_to_test, file_to_test, False, True)
+        result = validate(directory_to_test, file_to_test, False, True, [WorkflowVendorValidator()], True)
         self.assertTrue(result)
 
     def test_version_validator(self):
         # Test bad workflows. This will test the workflow_version_validator
-        validater = Validate([WorkflowVersionValidator()], True)
         directory_to_test = "workflow_examples/version_tests"
         file_to_test = "workflow_version_too_low.spec.yaml"
-        result = validater.validate(directory_to_test, file_to_test, False, True)
+        result = validate(directory_to_test, file_to_test, False, True, [WorkflowVersionValidator()], True)
         self.assertTrue(result)
 
     def test_support_validator(self):
         # Test bad workflows. This will test the workflow_support_validator
-        validater = Validate([WorkflowSupportValidator()], True)
         directory_to_test = "workflow_examples/support_tests"
         file_to_test = "workflow_no_supporter.spec.yaml"
-        result = validater.validate(directory_to_test, file_to_test, False, True)
+        result = validate(directory_to_test, file_to_test, False, True, [WorkflowSupportValidator()], True)
         self.assertTrue(result)
 
     def test_extension_validator(self):
         # Test bad workflows. This will test the workflow_extension_validator
-        validater = Validate([WorkflowExtensionValidator()], True)
         directory_to_test = "workflow_examples/extension_tests"
         file_to_test = "workflow_extension_not_workflow.spec.yaml"
-        result = validater.validate(directory_to_test, file_to_test, False, True)
+        result = validate(directory_to_test, file_to_test, False, True, [WorkflowExtensionValidator()], True)
         self.assertTrue(result)
 
     def test_help_validator(self):
         # Test bad workflows. This will test the workflow_help_validator
-        validater = Validate([WorkflowHelpValidator()], True)
         directory_to_test = "workflow_examples/help_tests"
         file_to_test = "workflow.spec.yaml"
-        result = validater.validate(directory_to_test, file_to_test, False, True)
+        result = validate(directory_to_test, file_to_test, False, True, [WorkflowHelpValidator()], True)
         self.assertTrue(result)
 
     def test_change_log_validator(self):
         # Test bad workflows. This will test the workflow_change_log_validator
-        validater = Validate([WorkflowChangelogValidator()], True)
         directory_to_test = "workflow_examples/change_log_tests"
         file_to_test = "workflow.spec.yaml"
-        result = validater.validate(directory_to_test, file_to_test, False, True)
+        result = validate(directory_to_test, file_to_test, False, True, [WorkflowChangelogValidator()], True)
         self.assertTrue(result)
 
     def test_files_validator(self):
         # Test bad workflows. This will test the workflow_files_validator
-        validater = Validate([WorkflowFilesValidator()], True)
         directory_to_test = "workflow_examples/files_tests"
         file_to_test = "workflow.spec.yaml"
-        result = validater.validate(directory_to_test, file_to_test, False, True)
+        result = validate(directory_to_test, file_to_test, False, True, [WorkflowFilesValidator()], True)
         self.assertTrue(result)
 
     def test_profanity_validator(self):
         # Test bad workflows. This will test the workflow_profanity_validator
-        validater = Validate([WorkflowProfanityValidator()], True)
         directory_to_test = "workflow_examples/profanity_tests"
         file_to_test = "workflow_profanity.spec.yaml"
-        result = validater.validate(directory_to_test, file_to_test, False, True)
+        result = validate(directory_to_test, file_to_test, False, True, [WorkflowProfanityValidator()], True)
         self.assertTrue(result)
