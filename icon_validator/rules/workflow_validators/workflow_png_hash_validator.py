25ca8585bee92e873243d5c823bc66c0b4eb65d7
Matthew Hofert <32648900+mhofert-r7@users.noreply.github.com>
2020-01-30 11:08:53 -0600

    Update icon_validator/rules/workflow_validators/workflow_png_hash_validator.py
    Co-Authored-By: Luke Milby <32271028+lmilby-r7@users.noreply.github.com>

--- icon_validator/rules/workflow_validators/workflow_png_hash_validator.py
+++ icon_validator/rules/workflow_validators/workflow_png_hash_validator.py
@@ -18,5 +18,5 @@ class WorkflowPNGHashValidator(KomandPluginValidator):
         if not hash_ == self._GOOD_HASH:
             raise ValidationException(f"The extention.png file in the workflow directory is incorrect."
                                       f" The File should have a SHA2 hash of"
-                                      f" 02094da9b8d40d9411eb10f45cb1cd1627d24bd7006bc105375b00a97ea66d3e."
+                                      f" {self._GOOD_HASH}."
                                       f" The files hash was {hash_}")
