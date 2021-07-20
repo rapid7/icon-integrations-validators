947de1e367d3acf64825ce46fa4e425c5b2a585a
Aine <aine_holland@rapid7.com>
2021-07-20 16:21:42 -0400

    created a supported versions validator

---
+++ icon_validator/rules/plugin_validators/supported_version_validator.py
@@ -0,0 +1,18 @@
+import re
+
+from icon_validator.rules.validator import KomandPluginValidator
+from icon_validator.exceptions import ValidationException
+import requests
+
+
+class SupportedVersionValidator(KomandPluginValidator):
+
+    @staticmethod
+    def validate_spec(spec):
+        if "supported_versions" not in spec.spec_dictionary():
+            raise ValidationException("Plugin supported_versions is missing.")
+        if not isinstance(spec.spec_dictionary()["supported_versions"], list):
+            raise ValidationException("Plugin supported_versions does not contain a list of values.")
+
+    def validate(self, spec):
+        SupportedVersionValidator.validate_spec(spec)
