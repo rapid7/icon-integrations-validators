b7d9a3e30120718618d107b65ea1247cd46103a3
Luke Milby <luke_milby@rapid7.com>
2021-07-22 15:23:17 -0500

    add validator for python script use

---
+++ icon_validator/rules/workflow_validators/workflow_python_script_use_validator.py
@@ -0,0 +1,51 @@
+import os
+import json
+from typing import List
+from icon_validator.rules.validator import KomandPluginValidator
+from icon_validator.exceptions import ValidationException
+from icon_validator.workflow.marshal import read_workflow
+from icon_validator.workflow.model import Workflow
+
+
+class WorkflowPythonScriptUseValidator(KomandPluginValidator):
+    @staticmethod
+    def python_plugin_used(workflow: Workflow) -> List[dict]:
+        """
+        python_plugin_used checks if the python plugin is used with node name
+        and plugin
+        :param workflow: dataclass workflow
+        :return: list of steps that use the python plugin
+        """
+        findings = []
+        wf = workflow.kom.get_latest_workflow_version()
+        plugins = wf.get_plugins_used()
+        for plug in plugins:
+            if "Python 3 Script" in plug["plugin_name"]:
+                findings.append(plug)
+        return findings
+
+    def validate(self, spec):
+        """
+        Checks if the workflow contains the usage of the python script plugin and warns
+        of its use
+        """
+        wf = dict()
+        d = spec.directory
+        for file_name in os.listdir(d):
+            if file_name.endswith(".icon"):
+                try:
+                    wf = read_workflow(spec=spec, file_name=file_name)
+                except json.JSONDecodeError:
+                    raise ValidationException(
+                        "ICON file is not in JSON format try exporting the .icon file again"
+                    )
+
+        findings = WorkflowPythonScriptUseValidator.python_plugin_used(wf)
+
+        if len(findings) > 0:
+            results = "Python Plugin use found in these steps\n"
+
+            for f in findings:
+                results += f'\t\t[*] Step Name - {f["node_name"]}\n'
+            results += "\tPlease review these steps carefully before use"
+            raise ValidationException(results)
