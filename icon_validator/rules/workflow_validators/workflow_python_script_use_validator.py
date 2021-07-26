12adb47b7326aa4e3ff1db41715ef30bb3ea14d8
Luke Milby <luke_milby@rapid7.com>
2021-07-26 09:52:59 -0500

    review

--- icon_validator/rules/workflow_validators/workflow_python_script_use_validator.py
+++ icon_validator/rules/workflow_validators/workflow_python_script_use_validator.py
@@ -9,8 +9,10 @@ from icon_validator.styling import YELLOW
 
 
 class WorkflowPythonScriptUseValidator(KomandPluginValidator):
-    @staticmethod
-    def python_plugin_used(workflow: Workflow) -> List[dict]:
+
+    _PLUGIN_NAME = "Python 3 Script"
+
+    def python_plugin_used(self, workflow: Workflow) -> List[dict]:
         """
         python_plugin_used checks if the python plugin is used with node name
         and plugin
@@ -21,7 +23,7 @@ class WorkflowPythonScriptUseValidator(KomandPluginValidator):
         wf = workflow.kom.get_latest_workflow_version()
         plugins = wf.get_plugins_used()
         for plug in plugins:
-            if "Python 3 Script" in plug["plugin_name"]:
+            if self._PLUGIN_NAME in plug["plugin_name"]:
                 findings.append(plug)
         return findings
 
@@ -38,13 +40,13 @@ class WorkflowPythonScriptUseValidator(KomandPluginValidator):
                     wf = read_workflow(spec=spec, file_name=file_name)
                 except json.JSONDecodeError:
                     raise ValidationException(
-                        "ICON file is not in JSON format try exporting the .icon file again"
+                        "ICON file is not in JSON format try exporting the workflow file again"
                     )
 
         findings = WorkflowPythonScriptUseValidator.python_plugin_used(wf)
 
         if len(findings) > 0:
-            results = f"\t{YELLOW}Python Plugin use found in these steps\n"
+            results = f"\t{YELLOW}{self._PLUGIN_NAME} use found in these steps\n"
 
             for f in findings:
                 results += f'\t\t[-] Step Name - {f["node_name"]}\n'
--- icon_validator/workflow/model.py
+++ icon_validator/workflow/model.py
@@ -2,15 +2,6 @@ from typing import List, Optional, Type, Union
 from dataclasses import dataclass, field
 
 
-class Templates:
-    """
-    Templates is a model thats used for managing the template development lifecycle
-    """
-
-    # TODO Create template struct
-    pass
-
-
 # Kom/Icon file
 @dataclass
 class TriggersInputActorProperties:
@@ -76,7 +67,7 @@ class WorkflowVersion:
         """
         get_step_contents parses the workflow version steps and grabs the
         contents of each step
-        :return:
+        :return:  List of step contents as dictionaries
         """
         content = []
         for step, value in self.steps.items():
@@ -87,7 +78,7 @@ class WorkflowVersion:
         """
         get_plugin_steps filters a collection of steps that contain the use of
         a plugin
-        :return: list of steps that contain a plugin
+        :return: List of steps that contain a plugin
         """
         steps = []
         steps_content = self.get_steps_contents()
@@ -100,29 +91,30 @@ class WorkflowVersion:
         """
         get_plugins_used fetches a collection of  dicts containing node name
          and the plugin used in that node
-        :return: list of dicts containing 'node_name' and 'plugin_name'
+        :return: List of dicts containing 'node_name' and 'plugin_name'
         """
         plugins = []
         plugin_steps = self.get_plugin_steps()
         for step in plugin_steps:
-            plugins.append({
-                "plugin_name": step["plugin"]["name"],
-                "node_name": step["name"]
-            })
+            plugins.append(
+                {"plugin_name": step["plugin"]["name"], "node_name": step["name"]}
+            )
         return plugins
 
 
 @dataclass
 class Kom:
     workflowVersions: List[WorkflowVersion] = field(
-        default_factory=lambda: [WorkflowVersion])
+        default_factory=lambda: [WorkflowVersion]
+    )
     triggers: List[Trigger] = field(default_factory=lambda: [Trigger])
     komandVersion: str = ""
     komFileVersion: str = ""
     exportedAt: str = ""
 
-    def get_latest_workflow_version(self) -> Union[
-        Type[WorkflowVersion], WorkflowVersion]:
+    def get_latest_workflow_version(
+        self,
+    ) -> Union[Type[WorkflowVersion], WorkflowVersion]:
         if len(self.workflowVersions) <= 0:
             return WorkflowVersion
         return self.workflowVersions[0]
