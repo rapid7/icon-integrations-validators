b3ae81842c8d0bdc17620cfb607505aae894172e
Luke Milby <32271028+lmilby-r7@users.noreply.github.com>
2021-07-26 13:28:59 -0500

    Python Script Plugin validator (#135)
    * add python script validator to init
    
    * add dataclass and library needed to serialize a workflow to the dataclass
    
    * add validator for python script use
    
    * add test and files for test
    
    * version and changelog
    
    * black formatting
    
    * validator warns
    
    * remove prints
    
    * review
    
    * Update icon_validator/rules/workflow_validators/workflow_python_script_use_validator.py
    
    * update self reference
    
    Co-authored-by: Mike Rinehart <32079048+mrinehart-r7@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="insightconnect_integrations_validators",
-    version="2.39.0",
+    version="2.40.0",
     description="Validator tooling for InsightConnect integrations",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -24,6 +24,7 @@ setup(
         "urlextract==0.14.0",
         "typing==3.7.4.1",
         "PyYAML~=5.4.1",
+        "dacite==1.6.0",
     ],
     entry_points={"console_scripts": ["icon-validate=icon_validator.__main__:main"]},
     classifiers=[
