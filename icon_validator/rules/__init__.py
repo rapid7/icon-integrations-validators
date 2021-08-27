f31c68dada417d56146b362dca3a6177f190c81a
Mike Rinehart <mike_rinehart@rapid7.com>
2021-08-27 13:32:51 -0500

    Add TemplateWorkflowVersionValidator

--- icon_validator/rules/__init__.py
+++ icon_validator/rules/__init__.py
@@ -70,10 +70,12 @@ from icon_validator.rules.workflow_validators.workflow_parameters_keyword_valida
 
 # template imports
 from icon_validator.rules.template_validators.template_id_validator import *
+from icon_validator.rules.template_validators.template_workflow_version_validator import *
 
 # The order of this list is the execution order of the validators.
 TEMPLATE_VALIDATORS = [
-    TemplateIDValidator()
+    TemplateIDValidator(),
+    TemplateWorkflowVersionValidator(),
 ]
 
 
---
+++ icon_validator/rules/template_validators/template_workflow_version_validator.py
@@ -0,0 +1,23 @@
+from icon_validator.rules.validator import KomandPluginValidator
+from icon_validator.exceptions import ValidationException
+from icon_validator.workflow.model import Workflow
+from icon_validator.workflow.unmarshal import read_workflow
+
+
+class TemplateWorkflowVersionValidator(KomandPluginValidator):
+
+    def version_exists(self, wf: Workflow) -> bool:
+        if wf.kom.workflowVersions[0].version:
+            return True
+        return False
+
+    def validate(self, spec):
+        """
+        Validates template ensuring ID
+        """
+        wf: Workflow = read_workflow(spec=spec)
+
+        if not self.version_exists(wf=wf):
+            raise ValidationException(
+                "Template Validator: Template missing workflow version"
+            )
