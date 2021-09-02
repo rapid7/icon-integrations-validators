cf33d1642b6f45209a2d49e91f6930ddef4b5ae4
Jasmine Rose <jasmine_rose@rapid7.com>
2021-09-02 13:36:25 -0400

    parse_obj_as in try-catch

--- icon_validator/workflow/unmarshal.py
+++ icon_validator/workflow/unmarshal.py
@@ -7,7 +7,7 @@ from pydantic import parse_obj_as
 
 def read_workflow(spec: KomandPluginSpec, file_name: str = None) -> Workflow:
     """
-    read_workflow takes a raw file and tries to load into a Workflow dataclass
+    read_workflow takes a raw file and tries to load into a Workflow pydantic model
     :param spec: workflow spec
     :param file_name: workflow file ending in kom or icon
     :return: Workflow dataclass
@@ -24,8 +24,9 @@ def read_workflow(spec: KomandPluginSpec, file_name: str = None) -> Workflow:
     with open(os.path.join(spec.directory, file_name)) as workflow_file:
         try:
             wf = json.load(workflow_file)
+            return parse_obj_as(Workflow, wf)
         except Exception as e:
-            raise Exception('Unable to read workflow', e)
-        return parse_obj_as(Workflow, wf)
+            raise Exception('Unable to read and parse workflow', e)
+
 
 
