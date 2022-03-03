82819cee5feae13227d48b8ba143917cf6eeae43
Aine <aine_holland@rapid7.com>
2022-03-03 09:41:25 -0500

    a change

--- icon_validator/rules/plugin_validators/help_input_output_validator.py
+++ icon_validator/rules/plugin_validators/help_input_output_validator.py
@@ -96,11 +96,8 @@ class HelpInputOutputValidator(KomandPluginValidator):
             name_ = k
             type_ = output_content.get(k).get("type")
             required = output_content.get(k).get("required", False)
-            description = output_content.get(k).get("description", None)
-            example = output_content.get(k).get("example", None)
-            if isinstance(example, list):
-                example = f"{example}".replace("'", '"')
-            action_output.append(f"|{name_}|{type_}|{required}|{description}|{example}|")
+            description = output_content.get(k).get("description")
+            action_output.append(f"|{name_}|{type_}|{required}|{description}|")
         return action_output
 
     def validate(self, spec):
