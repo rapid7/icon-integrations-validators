fac0fd747a7d75f8df29d6400077473f3b104e6b
conor-mcnally <conor_mcnally@rapid7.com>
2022-11-25 15:05:41 +0000

    Update version and changelog

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="insightconnect_integrations_validators",
-    version="2.46.3",
+    version="2.46.4",
     description="Validator tooling for InsightConnect integrations",
     long_description=long_description,
     long_description_content_type="text/markdown",
