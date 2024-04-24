b17d4ea1caeef0a9b3a864285646291a5f8f2e28
igorski-r7 <igor_gorski@rapid7.com>
2024-04-24 14:51:10 +0200

    RuntimeValidator - 16777 - Added SDK version validation

--- icon_validator/rules/plugin_validators/runtime_validator.py
+++ icon_validator/rules/plugin_validators/runtime_validator.py
@@ -1,8 +1,12 @@
-import os
 import glob
+import os
+import re
+from typing import List, Union
 
-from icon_validator.rules.validator import KomandPluginValidator
+import requests
+from icon_validator.constants import DEFAULT_TIMEOUT
 from icon_validator.exceptions import ValidationException
+from icon_validator.rules.validator import KomandPluginValidator
 
 
 class RuntimeValidator(KomandPluginValidator):
@@ -45,31 +49,71 @@ class RuntimeValidator(KomandPluginValidator):
             for path in paths:
                 for root, dirs, files in os.walk(path):
                     for file in files:
-                        with open(os.path.join(root, file), 'r') as open_file:
+                        with open(os.path.join(root, file), "r") as open_file:
                             file_str = open_file.read().replace("\n", "")
 
                             if "cache" in file_str:
-                                raise ValidationException(f"Cloud ready plugins cannot contain caching. "
-                                                          f"Update {str(os.path.join(root, file))}.")
+                                raise ValidationException(
+                                    f"Cloud ready plugins cannot contain caching. "
+                                    f"Update {str(os.path.join(root, file))}."
+                                )
 
-    @staticmethod
-    def validate_dockerfile(spec, latest_images):
+    def validate_dockerfile(self, spec, latest_images):
         if "setup.py" in os.listdir(spec.directory):
             with open(f"{spec.directory}/setup.py", "r") as setup_file:
                 setup_str = setup_file.read().replace("\n", "")
 
                 if "insightconnect-plugin-runtime" in setup_str:
                     with open(f"{spec.directory}/Dockerfile", "r") as docker_file:
-                        docker_str = docker_file.read().replace("\n", "")
+                        docker_str = docker_file.read()
+                        docker_image = next(
+                            filter(lambda image: image in docker_str, latest_images),
+                            None,
+                        )
 
                         if not any(image in docker_str for image in latest_images):
-                            raise ValidationException("insightconnect-plugin-runtime is being used in setup.py. "
-                                                      "Update Dockerfile accordingly to use latest base image.")
+                            raise ValidationException(
+                                "insightconnect-plugin-runtime is being used in setup.py. "
+                                "Update Dockerfile accordingly to use latest base image."
+                            )
+
+                        current_tag = self._parse_image_tag(docker_image, docker_str)
+                        latest_tags = self._get_latest_runtime_tags(docker_image)
+                        if current_tag and current_tag not in latest_tags:
+                            raise ValidationException(
+                                f"The current base image tag ({current_tag}) set is not latest. "
+                                f"Please update Dockerfile accordingly to use latest base image."
+                                f" Current latest tags are: {latest_tags}" if latest_tags else ""
+                            )
+
+    @staticmethod
+    def _parse_image_tag(image_name: str, dockerfile_content: str) -> Union[str, None]:
+        match = re.search(f"{image_name}:\s*(.*)", dockerfile_content)
+        if match:
+            return match.group(1)
+        return None
+
+    @staticmethod
+    def _get_latest_runtime_tags(image_name: str) -> List[str]:
+        latest_tags = ["latest"]
+        try:
+            response = requests.request(
+                "GET",
+                f"https://hub.docker.com/v2/repositories/{image_name}/tags",
+                params={"page_size": 3, "ordering": "last_updated"},
+                timeout=DEFAULT_TIMEOUT,
+            )
+            response.raise_for_status()
+            return latest_tags + [tag.get("name") for tag in response.json().get("results", [])]
+        except Exception:
+            return latest_tags
 
     def validate(self, spec):
-        latest_images = ["rapid7/insightconnect-python-3-plugin",
-                         "rapid7/insightconnect-python-3-slim-plugin"]
-        RuntimeValidator.validate_dockerfile(spec, latest_images)
+        latest_images = [
+            "rapid7/insightconnect-python-3-plugin",
+            "rapid7/insightconnect-python-3-slim-plugin",
+        ]
+        self.validate_dockerfile(spec, latest_images)
 
         with open(f"{spec.directory}/Dockerfile", "r") as file:
             docker_str = file.read().replace("\n", "")
